323f66ececd2aa06b61f5c9f8676bad3ca39bb01
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-10-29 22:20:59 -0500

    :recycle: (eris) Corrección en la gestión de errores

--- eeris/eris.go
+++ eeris/eris.go
@@ -1,11 +1,28 @@
-package errors
+package eeris
 
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/rotisserie/eris"
 )
+
+type PrinterError interface {
+	Error(...string)
+}
+
+type defaultPrinter struct{}
+
+func (d defaultPrinter) Error(content ...string) {
+	c := make([]any, len(content))
+	for i, v := range content {
+		c[i] = v
+	}
+	log.Fatalf(content[0], c[1:]...)
+}
 
 func NewError(cls, msg string) error {
 	return eris.New(fmt.Sprintf("[%s] %s", cls, msg))
@@ -15,15 +32,21 @@ func NewWrapError(err error, cls, msg string) error {
 	return eris.Wrap(err, fmt.Sprintf("[%s] %s", cls, msg))
 }
 
-func TraceString(err error) string {
+func TraceAndExit(err error, printer PrinterError) {
 	format := eris.NewDefaultStringFormat(eris.FormatOptions{
 		InvertOutput: true,
 		WithTrace:    true,
 		InvertTrace:  true,
 	})
 
-	// format the error to a string and return
-	return eris.ToCustomString(err, format)
+	strerr := eris.ToCustomString(err, format)
+
+	p := printer
+	if p == nil {
+		p = defaultPrinter{}
+	}
+	p.Error(strings.Split(strerr, "\n")...)
+	os.Exit(3)
 }
 
 func ToJsonString(err error, indent bool) string {
