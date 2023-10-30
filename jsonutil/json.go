7302489f426b37fbc604251a83cd3c1bc1ac145d
Walter Fabián Rodríguez Salazar <wfabianrs@gmail.com>
2023-10-29 22:22:55 -0500

    :recycle: (json) Modificación en la gestión de los errores

--- jsonutil/json.go
+++ jsonutil/json.go
@@ -2,20 +2,20 @@ package jsonutil
 
 import (
 	"encoding/json"
-	gerror "errors"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"strings"
 
-	"github.com/wfrodriguez/mimir/errors"
+	"github.com/wfrodriguez/mimir/eeris"
 )
 
 // ReadJSON lee un archivo de la ruta `path` y lo asigna en la variable `dst`
 func ReadJSON(path string, dst any) error { // {{{
 	body, err := os.ReadFile(path)
 	if err != nil {
-		return errors.NewWrapError(err, "ReadJSON", fmt.Sprintf("lectura del archivo `%s`", path))
+		return eeris.NewWrapError(err, "ReadJSON", fmt.Sprintf("lectura del archivo `%s`", path))
 	}
 	dec := json.NewDecoder(strings.NewReader(string(body)))
 	dec.DisallowUnknownFields()
@@ -26,17 +26,17 @@ func ReadJSON(path string, dst any) error { // {{{
 		var invalidUnmarshalError *json.InvalidUnmarshalError
 
 		switch {
-		case gerror.As(err, &syntaxError):
-			return errors.NewWrapError(
+		case errors.As(err, &syntaxError):
+			return eeris.NewWrapError(
 				syntaxError,
 				"ReadJSON",
 				fmt.Sprintf("El texto contiene JSON mal formado (en el carácter %d)", syntaxError.Offset),
 			)
-		case gerror.Is(err, io.ErrUnexpectedEOF):
-			return errors.NewWrapError(err, "ReadJSON", "El texto contiene JSON mal formado")
-		case gerror.As(err, &unmarshalTypeError):
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return eeris.NewWrapError(err, "ReadJSON", "El texto contiene JSON mal formado")
+		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
-				return errors.NewWrapError(
+				return eeris.NewWrapError(
 					unmarshalTypeError,
 					"ReadJSON",
 					fmt.Sprintf(
@@ -45,7 +45,7 @@ func ReadJSON(path string, dst any) error { // {{{
 					),
 				)
 			}
-			return errors.NewWrapError(
+			return eeris.NewWrapError(
 				unmarshalTypeError,
 				"ReadJSON",
 				fmt.Sprintf(
@@ -53,8 +53,8 @@ func ReadJSON(path string, dst any) error { // {{{
 					unmarshalTypeError.Offset,
 				),
 			)
-		case gerror.Is(err, io.EOF):
-			return errors.NewWrapError(err, "ReadJSON", "El texto no debe estar vacío")
+		case errors.Is(err, io.EOF):
+			return eeris.NewWrapError(err, "ReadJSON", "El texto no debe estar vacío")
 		// Si el JSON contiene un campo que no puede ser mapeado al destino entonces Decode() devolverá ahora un mensaje
 		// de error con el formato "json: unknown field "<name>". Comprobamos esto, extraemos el nombre del campo del
 		// error y lo interpolamos en nuestro mensaje de error personalizado. Tenga en cuenta que hay un tema abierto en
@@ -62,21 +62,21 @@ func ReadJSON(path string, dst any) error { // {{{
 		// futuro.
 		case strings.HasPrefix(err.Error(), "json: unknown field "):
 			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			return errors.NewWrapError(
+			return eeris.NewWrapError(
 				err,
 				"ReadJSON",
 				fmt.Sprintf("El texto contiene una clave desconocida `%s`", fieldName),
 			)
-		case gerror.As(err, &invalidUnmarshalError):
-			return errors.NewWrapError(err, "ReadJSON", "")
+		case errors.As(err, &invalidUnmarshalError):
+			return eeris.NewWrapError(err, "ReadJSON", "")
 		default:
-			return errors.NewWrapError(err, "ReadJSON", "Error general")
+			return eeris.NewWrapError(err, "ReadJSON", "Error general")
 		}
 	}
 
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
-		return errors.NewWrapError(err, "ReadJSON", "El texto sólo debe contener un único valor JSON")
+		return eeris.NewWrapError(err, "ReadJSON", "El texto sólo debe contener un único valor JSON")
 	}
 
 	return nil
